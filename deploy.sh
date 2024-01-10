6ba641435478b255145f2979acdb87698e220516
Randall Melton <rmeltonmi@gmail.com>
2024-01-10 12:11:13 -0500

    Update Date

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 echo "Deploy introlinux website to Production"
 cp -r . /var/www/introlinux
 # cp -r ./introlinux /var/www/introlinux
-echo "Website was copied"
+echo "Website was copied Wed, Jan 10th, 2024"
